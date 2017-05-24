529079f9b06990c5498c3efe603e6a2a80177f89
longjiazuo <812198178@qq.com>
2017-05-24 16:46:18 +0800

    增加Spring Boot的配置文件的示例代码

---
+++ springBoot-basic/springBoot-configFile/src/main/java/org/light4j/springboot/configFile/controller/HelloController.java
@@ -0,0 +1,13 @@
+package org.light4j.springboot.configFile.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello Spring Boot";
+	}
+}
